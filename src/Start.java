e407b06d9ce2238b55cd31f3aba45d20607c3ce9
George Camilar <georgecamilar@Georges-MacBook-Pro.local>
2020-10-25 17:13:26 +0200

    Creating setup for lexical analyzer

--- src/Start.java
+++ src/Start.java
@@ -1,23 +1,5 @@
-import common.Lexer;
-import common.LexicalAtom;
-
 import java.util.Map;
 
 public class Start {
 
-    public static void printMap(Map<String, LexicalAtom> map) {
-        for (String key : map.keySet()) {
-            System.out.println("key : " + key + " value : " + map.get(key));
-        }
-    }
-
-    public static void main(String[] args) {
-        Lexer lexer = new Lexer();
-        lexer.parseSourceFile("source2.c");
-
-
-        printMap(lexer.getAtoms());
-        lexer.writeToFile("atoms.txt", lexer.getAtoms());
-
-    }
 }
